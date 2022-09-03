2d2f7c0b6d4c420c26e84402f05da09f4456d508
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-03 16:55:59 +0200

    Link only for local, replaced by a href

--- src/components/Hedaer.js
+++ src/components/Hedaer.js
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
 const Hedaer = () => {
   return (
     <header>
       <nav>
         <h2>Walid Bellili</h2>
         <ul>
-          <Link to={"https://github.com/WalidBellili"}>GITHUB</Link>
-          <Link to={"https://www.linkedin.com/in/walidbellili/"}>LINKEDIN</Link>
+          <a href="https://github.com/WalidBellili">GITHUB</a>
+          <a href="https://www.linkedin.com/in/walidbellili/">LINKEDIN</a>
         </ul>
       </nav>
     </header>
--- src/components/SecondContainer.js
+++ src/components/SecondContainer.js
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
-
 const SecondContainer = () => {
   return (
     <article className="card-container">
       <div className="flex">
-        <Link to={""}>
-          <img src={require("../assets/img/ScreenCatalogue.jpg")} alt="" />
-        </Link>
-        <Link to={""}>
-          <img src={require("../assets/img/pokemonscreen.jpg")} alt="" />{" "}
-        </Link>
-        <Link to={""}>
-          <img src={require("../assets/img/got.jpg")} alt="" />
-        </Link>
+        <a href="https://aesthetic-mandazi-c51276.netlify.app">
+          <img src={require("../assets/img/got.jpg")} alt="got" />
+        </a>
+        <a href="https://random-pokemon-wb.netlify.app">
+          <img src={require("../assets/img/pokemonscreen.jpg")} alt="" />
+        </a>
+        <a href="https://spontaneous-concha-d8a90c.netlify.app/">
+          <img
+            src={require("../assets/img/ScreenCatalogue.jpg")}
+            alt="catalogue"
+          />
+        </a>
       </div>
     </article>
   );
