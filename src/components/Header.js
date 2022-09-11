f87ec0b7ac453d7e594286d4fe89ef5bb50e7e9d
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-11 19:58:32 +0200

    new portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Walid Bellili</h1>
+        <ul>
+          <li>efefe</li>
+          <li>efefe</li>
+          <li>efefe</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,11 @@
-import Contact from "../components/Contact";
-import Container from "../components/Container";
-import Hedaer from "../components/Hedaer";
-import SecondContainer from "../components/SecondContainer";
+import Description from "../components/Description";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <>
-      <Hedaer />
-      <section className="parent-container">
-        <Container />
-      </section>
-      <section className="second-container">
-        <SecondContainer />
-      </section>
-      <section className="third-container">
-        <Contact />
-      </section>
+      <Header />
+      <Description />
     </>
   );
 };
