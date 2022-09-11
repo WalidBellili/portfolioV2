f828ba2da97d16d64413b73181d8ef0528f7e4aa
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-11 21:31:10 +0200

    idea of contact ok

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,15 @@ const Contact = () => {
   return (
     <section className="contact">
       <h2>Contact</h2>
-      <article className="contact-container"></article>
+      <article className="contact-container">
+        <a href="https://github.com/WalidBellili">Github</a>
+        <img
+          src={require("../assets/img/walidbellili.jpg")}
+          alt="walid bellili"
+        />
+        <a href="https://www.linkedin.com/in/walidbellili/">Linkedin</a>
+        <a href="mailto:walid.07@hotmail.fr">walid.07@hotmail.fr</a>
+      </article>
     </section>
   );
 };
