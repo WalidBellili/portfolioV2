f9684cf5749d331f636fd88454770aacfd7adff8
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-12 14:29:21 +0200

    mix-blend-mode

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,24 @@ import React from "react";
 const About = () => {
   return (
     <>
-    
-    <section className="about">
-      <article className="img1-wrap">
-        <div className="img img1"></div>
-      </article>
-      <div className="h2-wrap">
-        <h2>A propos</h2>
-      </div>
-      <p>ezzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</p>
-    </section>
-    <div className="line-wrap">
+      <section className="about">
+        <article className="img1-wrap">
+          <div className="img img1">
+            <img
+              src={require("../assets/img/walidbellili.jpg")}
+              alt="walid bellili"
+              className="img1"
+            />
+          </div>
+        </article>
+        <div className="h2-wrap">
+          <h2>A propos</h2>
+        </div>
+        <p>ezzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</p>
+      </section>
+      <div className="line-wrap">
         <div className="line"></div>
-    </div>
+      </div>
     </>
   );
 };
