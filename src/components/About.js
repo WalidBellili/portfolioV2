8a3d50ae7c5fc10bb7dffba78a0b208e88b1956c
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-21 14:19:59 +0200

    test qr code

--- src/components/About.js
+++ src/components/About.js
@@ -13,29 +13,13 @@ const About = () => {
       </article>
       <article className="txt">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          perspiciatis eos ipsum laborum ex ad, eligendi est, libero,
-          exercitationem dignissimos inventore amet ducimus minus aperiam sint?
-          Aut beatae labore placeat, voluptas voluptatum cupiditate impedit
-          facilis sequi harum voluptate illum deleniti dignissimos, quasi
-          tenetur omnis, optio autem aliquid illo facere nesciunt quaerat iusto
-          dolore quas id. Perferendis ducimus quasi enim quos natus vel est
-          mollitia necessitatibus repellat illum eaque ut maxime voluptatem
-          laborum quaerat odit perspiciatis qui corporis, accusantium dolores
-          consequuntur eveniet. Impedit nulla, alias officia, molestiae totam
-          eos magnam mollitia, nisi assumenda ea voluptate voluptatem aliquid
-          saepe ipsam eveniet esse autem necessitatibus nobis iusto earum
-          explicabo optio sit? Sed quisquam delectus nisi libero aut repellat
-          repellendus accusamus aliquam at aliquid quas corrupti aperiam, eaque
-          quae deserunt minima dolores qui veniam eos temporibus voluptatem
-          possimus sint numquam! Iste eum, praesentium aut minus earum odio
-          labore omnis sit totam temporibus. Mollitia esse sunt libero soluta
-          reiciendis exercitationem corporis. Magni quo natus incidunt est
-          dolore aliquam voluptates enim eaque optio esse debitis id, expedita
-          nesciunt dolorem a illum possimus cum ea neque obcaecati provident
-          blanditiis nobis? Sint, necessitatibus unde magnam nulla quam
-          distinctio maiores sapiente quos, saepe, ipsam illo! Asperiores
-          aperiam voluptates maxime?
+          Ayant travaillé 10 année dans les brasseries Parisiennes, j'ai décidé
+          de donner un second souffle à ma vie professionnelle.
+        </p>
+        <p>
+          J'ai découvert l'univers du développement web en codant tout seul à la
+          maison. Puis j'ai découvert Konexio qui m'a permit de suivre un
+          bootcamp sur 3 mois en fullstack
         </p>
       </article>
       <div className="line-wrap">
