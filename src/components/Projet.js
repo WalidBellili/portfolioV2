0a817bcc9eee720cb9df0e13f6f87d27df14fdfc
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-10-07 22:27:37 +0200

    new project added

--- src/components/Projet.js
+++ src/components/Projet.js
@@ -20,7 +20,7 @@ const Projet = () => {
               <p>En cours</p>
             </div>
           </div>
-          <div className="projet" id="p3">
+          <div className="projet" id="p1">
             <img src={require("../assets/img/newGot.jpg")} alt="Moovice" />
             <div className="project-infos">
               <a href="https://illustrious-cascaron-656aa5.netlify.app">Got</a>
@@ -28,12 +28,15 @@ const Projet = () => {
             </div>
           </div>
           <div className="projet" id="p2">
-            <img src={require("../assets/img/Shifumi.jpg")} alt="Moovice" />
+            <img src={require("../assets/img/heros.jpg")} alt="heroes" />
             <div className="project-infos">
-              <a href="https://curious-gumption-aa625a.netlify.app/">Shifumi</a>
+              <a href="https://playful-nougat-fa6390.netlify.app/">
+                Hero FULLSTACK
+              </a>
               <p>En cours</p>
             </div>
           </div>
+
           <div className="projet" id="p3">
             <img src={require("../assets/img/Todo.jpg")} alt="Moovice" />
             <div className="project-infos">
@@ -41,6 +44,13 @@ const Projet = () => {
               <p>En cours</p>
             </div>
           </div>
+          {/* <div className="projet" id="p1">
+            <img src={require("../assets/img/Todo.jpg")} alt="Moovice" />
+            <div className="project-infos">
+              <a href="https://beamish-babka-3f3716.netlify.app/">TodoList</a>
+              <p>En cours</p>
+            </div>
+          </div> */}
         </article>
       </section>
       <div className="line-wrap">
